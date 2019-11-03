6ed076689ba46e67a2aba50d8ca2da6edc826257
Manas Khosla <43397881+ManasKhosla@users.noreply.github.com>
2019-11-03 21:12:43 +0530

    Add files via upload

---
+++ CC.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="CC.aspx.vb" Inherits="CollegePractical_VB.CC" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Convert: &nbsp;
+        <input type="text" id="US" runat="server" />
+            &nbsp; U.S. dollars to &nbsp;
+        <select style="width: 155px" id="Currency" runat="server"></select>
+            <br />
+            <br />
+            <input type="submit" value="OK" id="Convert" runat="server" /><br />
+            <br />
+            <div style="font-weight: bold" id="RESULT" runat="server"></div>
+        </div>
+
+    </form>
+</body>
+</html>
---
+++ CC.aspx.vb
@@ -0,0 +1,20 @@
+﻿Public Class CC
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If (Not Me.IsPostBack) Then
+            Currency.Items.Add(New ListItem(“Euro”, “1.12”))
+            Currency.Items.Add(New ListItem(“Japanese Yen”, “122.33”))
+            Currency.Items.Add(New ListItem(“Canadian Dollars”, ”1.58”))
+        End If
+    End Sub
+    Private Sub Convert_ServerClick(ByVal sender As Object, ByVal e As EventArgs) Handles Convert.ServerClick
+        Dim Amount As Decimal = Val(US.Value)
+        Dim Item As ListItem
+        Item = Currency.Items(Currency.SelectedIndex)
+        Dim NewAmount As Decimal = Amount * Val(Item.Value)
+        RESULT.InnerText = Amount.ToString() & " US Dollar = "
+        RESULT.InnerText &= NewAmount.ToString() & "" & Item.Text
+    End Sub
+
+End Class
